a979a9cb5ad89f207bce74c292ce4fb45ec2e88d
Avery Koranda <koran023@morris.umn.edu>
2018-09-03 14:03:55 -0500

    Create a folder for our Tree structure stuff called node_helper
    Fix return statements in IntNode so they properly return
    
    Remove .append from IntNode

---
+++ node_helper/IntNode.java
@@ -0,0 +1,28 @@
+package node_helper;
+
+public class IntNode{
+  private int value;
+  private IntNode parent;
+  private IntNode[] children;
+
+  public IntNode(int value, IntNode parent) {
+    this.value = value;
+    this.parent = parent;
+
+    parent.addChild(this);
+  }
+
+  private void addChild(IntNode newChild){
+    IntNode[] tempArray = new IntNode[this.children.length + 1];
+    for(int i = 0; i < this.children.length; i++){
+      tempArray[i] = this.children[i];
+    }
+    tempArray[tempArray.length - 1] = newChild;
+    this.children = tempArray;
+  }
+
+  public IntNode getParent(){ return this.parent; }
+  public IntNode[] getChildren(){ return this.children; }
+  public int getValue(){ return this.value; }
+  public void setValue(int newValue){ this.value = newValue; }
+}
